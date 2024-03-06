ad67f96b1901b1e03cdae3de04854943ed951cdf
AndySouth <southandy@gmail.com>
2024-03-06 17:30:15 +0000

    tweak dependencies to pass check for first time, woohoo !!

--- R/fish4myeloma-package.R
+++ R/fish4myeloma-package.R
@@ -9,5 +9,16 @@
 #' @importFrom dplyr rename_with
 #' @importFrom dplyr select
 
+#' @importFrom rlang .data
+
 #' @importFrom stringr str_detect
 #' @importFrom stringr str_locate
+#' @importFrom stringr str_sub
+#' @importFrom stringr fixed
+#'
+#'
+## usethis namespace: end
+#NULL
+
+#to get rid of check notes : "no visible binding for global variable"
+utils::globalVariables(c('cks1b_repeat'))
